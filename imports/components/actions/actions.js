46285e6ac65fe00333cf54d90fecbca87b640134
Huy Do <huy.do@wustl.edu>
2017-12-02 15:41:21 -0600

    a

--- imports/components/actions/actions.js
+++ imports/components/actions/actions.js
@@ -5,28 +5,7 @@ import template from './actions.html';
 
  
 class actionsCtrl {
-  chose(actionchose){
-    this.action = actionchose;
-    this.foodview = false;
-    this.watchview = false;
-    this.listenview = false;
-    if(actionchose == 'Eat'){
-    	this.foodview = true;
-    } else if(actionchose == 'Watch'){
-    	this.watchview = true;
-    } else{
-    	this.listenview = true;
-    }
-  }
-  showFood(){
-  	return this.foodview;
-  }
-  showWatch(){
-  	return this.watchview;
-  }
-  showListen(){
-  	return this.listenview;
-  }
+
 }
  
 export default angular.module('actions', [
